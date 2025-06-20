9e2975ba70718f24c67c194faed3845e6b581a96
Lucas <lgs.08lucas@gmail.com>
2025-06-20 20:19:22 -0300

    Criado classe para consumir API's

--- src/main/java/br/com/alura/fipesearch/FipeSearchApplication.java
+++ src/main/java/br/com/alura/fipesearch/FipeSearchApplication.java
@@ -1,13 +1,19 @@
 package br.com.alura.fipesearch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FipeSearchApplication {
+public class FipeSearchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FipeSearchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
     }
 
 }
