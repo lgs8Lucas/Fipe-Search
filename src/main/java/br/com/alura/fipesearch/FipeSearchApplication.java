c6154e228cc2f6bea7a493eb4e5590768a768cc9
Lucas <lgs.08lucas@gmail.com>
2025-06-20 20:11:10 -0300

    Criado projeto Fipe Search

---
+++ src/main/java/br/com/alura/fipesearch/FipeSearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.fipesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FipeSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/alura/fipesearch/FipeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.fipesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
